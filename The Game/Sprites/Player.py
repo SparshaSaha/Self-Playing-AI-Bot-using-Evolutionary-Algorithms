98a86d7209e79ec0af5541fbfd83ef10f810dc45
sparsha <saha.sparsha@gmail.com>
2018-07-22 22:34:02 +0530

    Score added, restart added and gamelogics changed

--- The Game/Sprites/Player.py
+++ The Game/Sprites/Player.py
@@ -3,16 +3,18 @@ class Player(object):
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.hitbox = (self.x, self.y, 41, 40)
+        self.hitbox = (self.x, self.y, 38, 40)
+        self.hitboxRect = pygame.Rect(self.x, self.y, 41, 40)
         self.imageName = "Sprites/GameImages/tRex.png"
 
     def drawCharacter(self, canvas):
         canvas.blit(pygame.image.load(self.imageName), (self.x,self.y))
-        self.hitbox = (self.x, self.y, 41, 40)
-        pygame.draw.rect(canvas, (255, 0, 0), self.hitbox,2)
+        self.hitbox = (self.x, self.y, 38, 40)
+        self.hitboxRect = pygame.Rect(self.x, self.y, 38, 40)
+        pygame.draw.rect(canvas, (255, 0, 0), self.hitbox, 2)
 
     def jump(self, jump, direction):
-        self.y += 2 * direction
+        self.y += 2.1 * direction
         if self.y < 510 :
             direction = 1
             return  True, direction
@@ -21,3 +23,6 @@ class Player(object):
             return False, direction
         else:
             return True, direction
+
+    def detectCollision(self, sprite):
+        return self.hitboxRect.colliderect(sprite.hitbox)
--- The Game/test.py
+++ The Game/test.py
@@ -8,7 +8,16 @@ from Sprites.CactusTriple import CactusTriple
 obstacleProbability = 0.5
 global obstaclesOnScreen
 obstaclesOnScreen = []
-speed = 1.5
+speed = 2
+score = 0
+font_name = pygame.font.match_font('arial')
+
+def drawText(surf, text, size, x, y):
+    font = pygame.font.Font(font_name, size)
+    text_surface = font.render(text, True, (0, 0, 0))
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (x, y)
+    surf.blit(text_surface, text_rect)
 
 def drawGameBackground():
     screen.fill(background_colour)
@@ -18,7 +27,6 @@ def drawCharacter():
     tRex.drawCharacter(screen)
     for obstacles in obstaclesOnScreen:
         obstacles.drawCharacter(screen)
-    pygame.display.update()
 
 def generateGameObstacles():
     if len(obstaclesOnScreen) == 0 or obstaclesOnScreen[len(obstaclesOnScreen) - 1].x < 1100:
@@ -31,16 +39,19 @@ def generateGameObstacles():
             else:
                 obstaclesOnScreen.append(CactusTriple(1370, 615))
 
-def cleanDeadObstaclesAndPropagate(obstacles):
-    index = 0;
+def cleanDeadObstaclesAndPropagate(obstacles, score):
+    index = 0
     for obstacle in obstacles:
         if obstacle.x >= 0:
             break
+        else:
+            score += 1
         index+=1
+
     obstacles = obstacles[index : ]
     for obstacle in obstacles:
         obstacle.propagate(speed)
-    return obstacles
+    return obstacles, score
 
 
 
@@ -62,6 +73,7 @@ tRex = Player(x, y)
 
 running = True
 jump = False
+gameOver = False
 while running:
     clock.tick(175)
     for event in pygame.event.get():
@@ -71,13 +83,24 @@ while running:
 
     keys = pygame.key.get_pressed()
 
+    if keys[pygame.K_r]:
+        gameOver = False
+        score = 0
+        obstaclesOnScreen = []
+
     if not jump:
         if keys[pygame.K_SPACE]:
             jump = True
     else:
         jump, direction = tRex.jump(jump, direction)
 
-    drawGameBackground()
-    generateGameObstacles()
-    obstaclesOnScreen = cleanDeadObstaclesAndPropagate(obstaclesOnScreen)
-    drawCharacter()
+    if not gameOver:
+        drawGameBackground()
+        generateGameObstacles()
+        obstaclesOnScreen, score = cleanDeadObstaclesAndPropagate(obstaclesOnScreen, score)
+        drawCharacter()
+        drawText(screen, 'score: ' + str(score), 30, 1200, 100)
+        pygame.display.update()
+
+    if len(obstaclesOnScreen) > 0 and tRex.detectCollision(obstaclesOnScreen[0]):
+        gameOver = True
