0790b8b609acbafcfbae4b20576d2ea43dd8562d
sparsha <saha.sparsha@gmail.com>
2018-08-22 00:43:35 +0530

    Game progressed..NEAT config file added

--- TheGameAndTheGeneticAI/Game.py
+++ TheGameAndTheGeneticAI/Game.py
@@ -2,6 +2,7 @@ import pygame
 import random
 import pickle
 import numpy as np
+import neat
 from Sprites.Player import Player
 from Sprites.CactusSingle import CactusSingle
 from Sprites.CactusDouble import CactusDouble
@@ -45,7 +46,7 @@ class Game(object):
     
     def drawCharacter(self):
         
-        self.trex.drawCharacter(self.screen)
+        self.trex.drawCharacter(self.screen, 0)
 
         for obstacles in self.obstaclesOnScreen:
             obstacles.drawCharacter(self.screen)
@@ -64,17 +65,34 @@ class Game(object):
     
     def detectCollisionAndKillTRex(self):
         if self.trex.detectCollision(self.obstaclesOnScreen[0]) and self.trex.alive:
-            self.trex.score = score
+            self.trex.score = self.score
             self.trex.alive = False
+            self.gameOver = True
 
     
-    def makeTrexsJump(self):
+    def makeTrexsJump(self, jmp):
         
         if not self.trex.isJumping:
-            if self.trex.predictedAction == 1:
+            if jmp:
                 self.trex.isJumping = True
         else:
             self.trex.isJumping, self.trex.direction = self.trex.jump(self.trex.isJumping, self.trex.direction, self.jumpSpeed)
+
+    
+
+    def cleanDeadObstaclesAndPropagate(self):
+        index = 0
+        for obstacle in self.obstaclesOnScreen:
+            if obstacle.x > 30:
+                break
+            else:
+                self.score += 1
+            index+=1
+
+        self.obstaclesOnScreen = self.obstaclesOnScreen[index : ]
+        for obstacle in self.obstaclesOnScreen:
+            obstacle.propagate(self.speed)
+        
     
 
     def game(self):
@@ -90,11 +108,36 @@ class Game(object):
                 if event.type == pygame.QUIT:
                     self.running = False
                     break
+
+            keys = pygame.key.get_pressed()
+
+            if keys[pygame.K_UP]:
+                self.makeTrexsJump(True)
+            else:
+                self.makeTrexsJump(False)
+            
+
+            if not self.gameOver:
+                self.drawGameBackground()
+                self.generateGameObstacles()
+                self.cleanDeadObstaclesAndPropagate()
+                self.drawCharacter()
+                self.drawText('score: ' + str(self.score), 20, 700, 50)
+            
+            pygame.display.update()
+
+            if len(self.obstaclesOnScreen) > 0:
+                self.detectCollisionAndKillTRex()
             
             
 
+                
+            
+            
+config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, 'config')
 g = Game()
-g.game()   
+g.game() 
+
     
 
 
